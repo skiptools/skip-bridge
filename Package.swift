79a78d406cdef72a397cd8eb4104bfea95221af9
Marc Prud'hommeaux <marc@skip.tools>
2024-10-18 14:59:21 -0400

    Use local Swift toolchain for swift build of the generated bridge

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
-        .package(url: "https://source.skip.tools/skip.git", from: "1.0.0"),
+        .package(url: "https://source.skip.tools/skip.git", branch: "main"),
         .package(url: "https://source.skip.tools/skip-lib.git", from: "1.0.0"),
     ],
     targets: [
