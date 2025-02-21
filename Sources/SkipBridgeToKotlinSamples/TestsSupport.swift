33378de929cfd4953b10b2672f454a5159b4f4d2
Abe White <abewhite@mac.com>
2025-02-20 23:03:01 -0600

    Fix AnyDynamicObject tests

--- Sources/SkipBridgeToKotlinSamples/TestsSupport.swift
+++ Sources/SkipBridgeToKotlinSamples/TestsSupport.swift
@@ -60,7 +60,7 @@ public func testSupport_dynamicJavaObjectProperties() throws -> Bool {
 
 public func testSupport_dynamicJavaObjectTraversal() throws -> Bool {
     let date = try AnyDynamicObject(className: "java.util.Date")
-    let string: String = try date.toInstant().toString()
+    let string: String = try date.toInstant()!.toString()!
     return !string.isEmpty
 }
 
@@ -82,7 +82,7 @@ public func testSupport_dynamicKotlinObjects() throws -> Bool {
     try dict.put(key: "a", value: 1) as Void
     try dict.put(key: "b", value: 2) as Void
     try dict.put(key: "c", value: 3) as Void
-    let count: Int = dict.keys.count!
+    let count: Int = dict.keys!.count!
     guard count == 3 else {
         return false
     }
@@ -103,18 +103,18 @@ public func testSupport_dynamicConverting() throws -> Bool {
     try arr.append(1) as Void
     try arr.append(2) as Void
     try arr.append(3) as Void
-    let size: Int = arr.count
+    let size: Int = arr.count!
     guard size == 3 else {
         return false
     }
 
-    let converted: [Int] = try arr.kotlin(nocopy: true) // .kotlin() returns List, which converts
+    let converted: [Int] = try arr.kotlin(nocopy: true)! // .kotlin() returns List, which converts
     guard converted == [1, 2, 3] else {
         return false
     }
 
     let url = try AnyDynamicObject(className: "java.net.URL", "https://skip.tools")
-    let uri: URL = try url.toURI() // java.net.URI converts
+    let uri: URL = try url.toURI()! // java.net.URI converts
     guard uri.absoluteString == "https://skip.tools" else {
         return false
     }
@@ -124,12 +124,12 @@ public func testSupport_dynamicConverting() throws -> Bool {
 
 public func testSupport_dynamicStatics() throws -> Bool {
     let math = try AnyDynamicObject(forStaticsOfClassName: "java.lang.Math")
-    let abs: Double = try math.abs(-1.0)
+    let abs: Double = try math.abs(-1.0)!
     guard abs == 1.0 else {
         return false
     }
 
-    let pi: Double = math.PI
+    let pi: Double = math.PI!
     guard Int(pi * 100.0) == 314 else {
         return false
     }
@@ -138,10 +138,10 @@ public func testSupport_dynamicStatics() throws -> Bool {
 
 #if os(Android) || ROBOLECTRIC
 public func testSupport_dynamicCodeGenJavaDateTime() throws -> Int64 {
-    return try K.java.util.Date().time
+    return try K.java.util.Date().time!
 }
 
 public func testSupport_dynamicCodeGenSkipDateTime() -> Double {
-    return K.skip.foundation.Date.Companion.now.timeIntervalSince1970
+    return K.skip.foundation.Date.Companion().now!.timeIntervalSince1970!
 }
 #endif
